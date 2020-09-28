dde4fcf791740ca09624e78d77643a65044252e1
sirvellen <kamikadze012@gmail.com>
2020-09-29 00:02:13 +0700

    pass valid

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'username' => 'nullable|string|min:1|max:24|regex:[^(?=.{1,24}$)(?![_.-])(?!.*[_.]{2})[a-zA-ZА-Яа-яЁё0-9._-]+(?<![_.-])$]',
             'email' => 'required|string|email:rfc,dns|unique:users,email|max:129',
-            'password' => ['required', 'string', 'min:8', 'max:24', 'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).(?=.*[$&+,:;=?@#|\'<>.-^*()%!]).{8,24}$/'],
+            'password' => ['required', 'string', 'min:8', 'max:24', 'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])*(?=.*[$&+,:;=?@#|\'<>.-^*()%!]).{8,24}$/'],
         ]);
 
         if ($validator->fails()) {
@@ -72,7 +72,7 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'username' => 'nullable|string|min:1|max:24|regex:[^(?=.{1,24}$)(?![_.-])(?!.*[_.]{2})[a-zA-Zа-яА-Я0-9._-]+(?<![_.-])$]',
             'email' => 'nullable|string|email:rfc,dns|unique:users,email|max:129',
-            'password' => ['nullable', 'string', 'min:8', 'max:24', 'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).(?=.*[$&+,:;=?@#|\'<>.-^*()%!]).{8,24}$/'],
+            'password' => ['nullable', 'string', 'min:8', 'max:24', 'regex:/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])*(?=.*[$&+,:;=?@#|\'<>.-^*()%!]).{8,24}$/'],
         ]);
 
         if ($validator->fails()) {
