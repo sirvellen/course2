4a650cd11811a6667ccdef339b1b2d95d1cffe08
sirvellen <kamikadze012@gmail.com>
2020-10-13 22:38:13 +0700

    projects timestamps

--- app/Project.php
+++ app/Project.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    public $timestamps = false;
+    public $timestamps = true;
     /**
      * The attributes that are mass assignable.
      *
